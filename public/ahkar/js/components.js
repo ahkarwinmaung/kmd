dc2b11ac31e600733775375f0b35dd242eacc665
AhKar Win Maung <ahkar@clickrmedia.com>
2022-06-27 11:48:08 +0630

    components update

--- public/ahkar/js/components.js
+++ public/ahkar/js/components.js
@@ -13,39 +13,45 @@ class Components    {
 
 
     bindHeader(callBack)    {
-        $.get(`${this.COMPONENTS_DIR}${this.COMPONENTS.header}`, header => {
-            let $target = $('#header-component');
-            $target.after(header);
-            $target.remove();
-    
-            if ( typeof callBack === 'function' ) callBack();
-        }, 'html');
+        let $target = $('#header-component');
+        if ( $target && $target.length )   {
+            $.get(`${this.COMPONENTS_DIR}${this.COMPONENTS.header}`, header => {
+                $target.after(header);
+                $target.remove();
+        
+                if ( typeof callBack === 'function' ) callBack();
+            }, 'html');
+        }
 
         return this;
     } // bindHeader() <-
 
 
     bindFooter(callBack)    {
-        $.get(`${this.COMPONENTS_DIR}${this.COMPONENTS.footer}`, footer => {
-            let $target = $('#footer-component');
-            $target.after(footer);
-            $target.remove();
-
-            if ( typeof callBack === 'function' ) callBack();
-        }, 'html');
+        let $target = $('#footer-component');
+        if ( $target && $target.length )   {
+            $.get(`${this.COMPONENTS_DIR}${this.COMPONENTS.footer}`, footer => {
+                $target.after(footer);
+                $target.remove();
+    
+                if ( typeof callBack === 'function' ) callBack();
+            }, 'html');
+        }
 
         return this;
     } // bindFooter() <-
 
 
     bindLoginModal(callBack)    {
-        $.get(`${this.COMPONENTS_DIR}${this.COMPONENTS.login_modal}`, login_modal => {
-            let $target = $('#login_modal-component');
-            $target.after(login_modal);
-            $target.remove();
-
-            if ( typeof callBack === 'function' ) callBack();
-        }, 'html');
+        let $target = $('#login_modal-component');
+        if ( $target && $target.length )   {
+            $.get(`${this.COMPONENTS_DIR}${this.COMPONENTS.login_modal}`, login_modal => {
+                $target.after(login_modal);
+                $target.remove();
+    
+                if ( typeof callBack === 'function' ) callBack();
+            }, 'html');
+        }
 
         return this;
     } // bindLoginModal() <-
